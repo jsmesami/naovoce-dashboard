d2934012e23d680c9e82a0d50ac5a43e7823c77d
Ondřej Nejedlý <jsmesami@gmail.com>
2023-01-12 19:28:36 +0100

    Format coordinates

--- dashboard/core/__init__.py
+++ dashboard/core/__init__.py
@@ -2,4 +2,10 @@ from flask import Blueprint
 
 core = Blueprint("core", __name__, cli_group="core")
 
+
+@core.app_template_filter()
+def format_coord(coord):
+    return "{:.10f}".format(coord).rjust(14, "\u2000") if type(coord) == float else "-"
+
+
 from . import commands, routes
