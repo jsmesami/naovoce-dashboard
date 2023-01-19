0e454b5d88b0691a57334057b440f6b72191c572
Ondřej Nejedlý <jsmesami@gmail.com>
2023-01-19 05:35:11 +0100

    Fix commands logger

--- dashboard/core/commands.py
+++ dashboard/core/commands.py
@@ -1,10 +1,9 @@
+import logging
+
+from flask import current_app
+
 from ..extensions import db
 from . import core, importer, newsletter
-
-
-@core.cli.command("update-db")
-def update_db():
-    importer.update_db(db)
 
 
 @core.cli.command("init-db")
@@ -12,6 +11,13 @@ def init_db():
     db.create_all()
 
 
+@core.cli.command("update-db")
+def update_db():
+    current_app.logger.setLevel(logging.INFO)
+    importer.update_db(db)
+
+
 @core.cli.command("update-newsletter")
 def update_newsletter():
+    current_app.logger.setLevel(logging.INFO)
     newsletter.update()
