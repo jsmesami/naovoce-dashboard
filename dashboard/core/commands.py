7c9cad17a54e4d0360d0758f511e95922e25b279
Ondřej Nejedlý <jsmesami@gmail.com>
2023-01-19 06:08:55 +0100

    Add command to dump emails

--- dashboard/core/commands.py
+++ dashboard/core/commands.py
@@ -8,16 +8,25 @@ from . import core, importer, newsletter
 
 @core.cli.command("init-db")
 def init_db():
+    """Initializes database with schema"""
     db.create_all()
 
 
 @core.cli.command("update-db")
 def update_db():
+    """Imports new data from Mapotic, executed daily with Cron"""
     current_app.logger.setLevel(logging.INFO)
     importer.update_db(db)
 
 
+@core.cli.command("dump-emails")
+def dump_emails():
+    """Dumps emails to stdout as CSV to be imported to Sendy"""
+    newsletter.dump_emails()
+
+
 @core.cli.command("update-newsletter")
 def update_newsletter():
+    """Subscribes new users to Sendy, executed daily with Cron"""
     current_app.logger.setLevel(logging.INFO)
     newsletter.update()
--- dashboard/core/newsletter.py
+++ dashboard/core/newsletter.py
@@ -3,21 +3,21 @@ from flask import current_app
 
 from ..extensions import db
 
-QUERY = """
-    SELECT
-    email,
-    TRIM(CONCAT(first_name, ' ', last_name)) as "name"
-    FROM creator
-    WHERE is_deleted = false
-    AND created > current_date - 1
-"""
-
 
 def update():
     nl_url = current_app.config.get("NEWSLETTER_URL")
     nl_list_id = current_app.config.get("NEWSLETTER_LIST_ID")
     nl_api_key = current_app.config.get("NEWSLETTER_API_KEY")
-    creators = db.session.execute(QUERY).mappings().all()
+
+    query = """
+        SELECT
+        TRIM(CONCAT(first_name, ' ', last_name)) as "name",
+        email
+        FROM creator
+        WHERE is_deleted = false
+        AND created > current_date - 1
+    """
+    creators = db.session.execute(query).mappings().all()
 
     current_app.logger.info("Running newsletter update")
 
@@ -37,3 +37,17 @@ def update():
             body |= {"name": name}
 
         requests.post(nl_url, data=body)
+
+
+def dump_emails():
+    query = """
+        SELECT
+        TRIM(CONCAT(first_name, ' ', last_name)) as "name",
+        email,
+        to_char(created, 'YYYY-MM-DD')
+        FROM creator
+        WHERE is_deleted = false
+        ORDER BY created DESC
+    """
+    for c in db.session.execute(query).all():
+        print(",".join(c))
