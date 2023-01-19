c5aca4418183e608c1ffe1998ab5c9958eb3d4a3
Ondřej Nejedlý <jsmesami@gmail.com>
2023-01-19 13:57:22 +0100

    Unsubscribe deleted users from newsletter

--- dashboard/core/newsletter.py
+++ dashboard/core/newsletter.py
@@ -4,11 +4,7 @@ from flask import current_app
 from ..extensions import db
 
 
-def update():
-    nl_url = current_app.config.get("NEWSLETTER_URL")
-    nl_list_id = current_app.config.get("NEWSLETTER_LIST_ID")
-    nl_api_key = current_app.config.get("NEWSLETTER_API_KEY")
-
+def subscribe(nl_url, nl_list_id, nl_api_key):
     query = """
         SELECT
         TRIM(CONCAT(first_name, ' ', last_name)) as "name",
@@ -25,7 +21,7 @@ def update():
         current_app.logger.info("No new creators")
         return
 
-    current_app.logger.info(f"Updating {len(creators)} creators")
+    current_app.logger.info(f"Subscribing {len(creators)} new creators")
 
     for c in creators:
         body = {
@@ -36,7 +32,42 @@ def update():
         if name := c["name"]:
             body |= {"name": name}
 
-        requests.post(nl_url, data=body)
+        requests.post(f"{nl_url}/subscribe", data=body)
+
+
+def unsubscribe(nl_url, nl_list_id, nl_api_key):
+    query = """
+        SELECT email
+        FROM creator
+        WHERE is_deleted = true
+        AND modified > current_date - 1
+    """
+    creators = db.session.execute(query).mappings().all()
+
+    if not creators:
+        current_app.logger.info("No deleted creators")
+        return
+
+    current_app.logger.info(f"Unsubscribing {len(creators)} deleted creators")
+
+    for c in creators:
+        body = {
+            "list": nl_list_id,
+            "api_key": nl_api_key,
+            "email": c["email"],
+        }
+
+        requests.post(f"{nl_url}/unsubscribe", data=body)
+
+
+def update():
+    nl_url = current_app.config.get("NEWSLETTER_URL")
+    nl_list_id = current_app.config.get("NEWSLETTER_LIST_ID")
+    nl_api_key = current_app.config.get("NEWSLETTER_API_KEY")
+
+    current_app.logger.info("Running newsletter update")
+    subscribe(nl_url, nl_list_id, nl_api_key)
+    unsubscribe(nl_url, nl_list_id, nl_api_key)
 
 
 def dump_emails():
